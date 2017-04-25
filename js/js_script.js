c71f630a117630d19246c588ae06a5d02069dd37
Moses Franco <mosesf@outlook.com>
2017-04-25 11:45:37 -0500

    Added a delay, that will show the modal, which honestly shouldn't be done, but okay.

---
+++ js/js_script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by mosesfranco on 4/25/17.
+ */
+(function () {
+    $(document).ready(function () {
+        "use strict";
+        // This was done to show a modal after a delay, but in truth, should never be done, because it's kinda frustrating'
+        setTimeout(function () {
+            $('#resumeModal').modal('show');
+        }, 8000)
+    })
+
+})();
