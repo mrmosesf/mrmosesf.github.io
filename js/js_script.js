939f89b6d20915682ca6f587294dc0f2976f76a3
Moses Franco <mosesf@outlook.com>
2017-05-04 10:29:52 -0500

    Touching portfolio, on mobile devices, no longer activates that link

--- js/js_script.js
+++ js/js_script.js
@@ -2,12 +2,20 @@
  * Created by mosesfranco on 4/25/17.
  */
 (function () {
-    $(document).ready(function () {
-        "use strict";
-        // This was done to show a modal after a delay, but in truth, should never be done, because it's kinda frustrating'
-        /*setTimeout(function () {
-            $('#resumeModal').modal('show');
-        }, 8000);*/
-    })
+
+    "use strict";
+    // This was done to show a modal after a delay, but in truth, should never be done, because it's kinda frustrating'
+    /*setTimeout(function () {
+     $('#resumeModal').modal('show');
+     }, 8000);*/
+
+    if ($(window).width() < 979) {
+        $('#specialDropdown').removeClass('disabled');
+        console.log("Class should be removed");
+    }
+    else {
+        $('#specialDropdown').addClass('disabled');
+        console.log("Dropdown is added");
+    }
 
 })();
